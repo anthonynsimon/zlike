18b0de8da77adaed50492354a6cf3e8bb6063a40
Anthony Najjar Simon <anthonynsimon@users.noreply.github.com>
2020-05-02 18:27:38 +0200

    Delegate aspect ratio and viewport handling to FitViewport class

--- core/src/com/anthonynsimon/zlike/ZLikeGame.java
+++ core/src/com/anthonynsimon/zlike/ZLikeGame.java
@@ -2,16 +2,13 @@ package com.anthonynsimon.zlike;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-
-import java.io.FileReader;
-import java.io.StringReader;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class ZLikeGame extends ApplicationAdapter {
     SpriteBatch batch;
@@ -19,12 +16,14 @@ public class ZLikeGame extends ApplicationAdapter {
     private TextureAtlas atlas;
     private Player player;
     private OrthographicCamera camera;
+    private Viewport viewport;
     private float stateTime = 0f;
 
     @Override
     public void create() {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, GameSettings.virtualWidth, GameSettings.virtualHeight);
+        viewport = new FitViewport(GameSettings.virtualWidth, GameSettings.virtualHeight, camera);
 
         batch = new SpriteBatch();
         atlas = new TextureAtlas(Gdx.files.internal("spritesheet.txt"));
@@ -42,10 +41,6 @@ public class ZLikeGame extends ApplicationAdapter {
         float deltaTime = Gdx.graphics.getDeltaTime();
         stateTime += deltaTime;
 
-        // Update camera projection matrix
-        camera.update();
-        batch.setProjectionMatrix(camera.combined);
-
         batch.begin();
         player.render(batch, stateTime);
         batch.end();
@@ -55,11 +50,8 @@ public class ZLikeGame extends ApplicationAdapter {
 
     @Override
     public void resize(int width, int height) {
-        // Resize camera viewport to keep aspect ratio and "zoom level"
-        super.resize(width, height);
-        camera.viewportWidth = GameSettings.virtualWidth;
-        camera.viewportHeight = GameSettings.virtualWidth * ((float) height / (float) width);
-        camera.update();
+        viewport.update(width, height, true);
+        batch.setProjectionMatrix(camera.combined);
     }
 
     @Override
